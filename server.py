41a3a864282e9d9e8dd03d712fc4ec071383e209
Harvindar Singh Garcha <garchaaman19@gmail.com>
2021-11-08 17:14:47 +0530

    removed unwanted comments

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from threading import Thread
 from zlib import compress
 from mss import mss
 
-# 截图大小
+
 WIDTH = int(1366 / 1)
 HEIGHT = int(768 / 1)
 
@@ -14,14 +14,9 @@ def send_screenshot(conn):
 
         while 'recording':
             img = sct.grab(rect)
-            # 压缩等级 (0-9)
-            # 并不一定是压缩level越高越好，这里要综合考虑压缩占用时间和网络传输时间
+            
             pixels = compress(img.bgra, 1)
-            # 压缩对比
-            # level:0 size:4196683
-            # level:1 size:248329
-            # level:2 size:246512
-            # level:9 size:196212
+            
             size = len(pixels)
             size_len = (size.bit_length() + 7) // 8
             conn.send(bytes([size_len]))
